82f4087b6dbbdd42d3b4c5d284c70b37d709d3ff
lastoriginbot <59411854+lastoriginbot@users.noreply.github.com>
2020-01-01 21:10:12 +0900

    Delete labiata.js

--- image/labiata.js
+++
@@ -1,1 +0,0 @@
-exports.img = "https://lh4.googleusercontent.com/IU69KP48J2uK-eSiUH5HpBWQ5kQxODABh2fqrdtC8VDfszp0Rzz-NA-oLvhpZvB8ohnzo5ovg6wY1a15xqmGrCfcx1bsAQDR0Sat3RQHXiHP2DzBB9upruLrMz2p9YnH4V5xbbsAXQ=w149-h149"

74a719fa8f59ec6b3246a0db138aae58a92021aa
anyabot <52899201+anyabot@users.noreply.github.com>
2020-01-01 12:21:28 +0900

    Update skill.js

--- commands/skill/skill.js
+++ commands/skill/skill.js
@@ -1,5 +1,8 @@
 const commando = require('discord.js-commando');
 const Discord = require('discord.js');
+var request = require('request');
+var cheerio = require('cheerio');
+var he = require('he');
 require('@gouch/to-title-case')
 var name = require('../../library/lib.js').name;
 class Skill extends commando.Command {
@@ -17,67 +20,76 @@ class Skill extends commando.Command {
         });
     }
 	async run(message, { text }) {
-    var unit = text.toLowerCase().toTitleCase();
-    unit = nameChange(unit);
-		var tr = unit.toLowerCase()
-		var path = '../../skill/' + tr + '.js'
-		var ipath = '../../image/' + tr + '.js'
-		if (moduleIsAvailable(path)) {
-			let embed = new Discord.RichEmbed()
-      let des
-      let img = require(ipath).img
-      embed.setThumbnail(img)
-      embed.setTitle(unit)
-      let na1 = require(path).na1
-      let sa1 = require(path).sa1
-      let ra1 = require(path).ra1
-      let aa1 = require(path).aa1
-      des = sa1 + "\n**Range/AP**: " + ra1 + "\n**AoE**: " + aa1
-      embed.addField("Active 1: " + na1, des);
-      let na2 = require(path).na2
-      let sa2 = require(path).sa2
-      let ra2 = require(path).ra2
-      let aa2 = require(path).aa2
-      des = sa2 + "\n**Range/AP**: " + ra2 + "\n**AoE**: " + aa2
-      embed.addField("Active 2: " + na2, des);
-      let np1 = require(path).np1
-      if (np1) {
-        let sp1 = require(path).sp1
-        let ap1 = require(path).ap1
-        des = sp1 + "\n**AoE**: " + ap1
-        embed.addField("Passive 1: " + np1, des);
-      }
-      let np2 = require(path).np2
-      if (np2) {
-        let sp2 = require(path).sp2
-        let ap2 = require(path).ap2
-        des = sp2 + "\n**AoE**: " + ap2
-        embed.addField("Passive 2: " + np2, des);
-      }
-      let np3 = require(path).np3
-      if (np3) {
-        let sp3 = require(path).sp3
-        let ap3 = require(path).ap3
-        des = sp3 + "\n**AoE**: " + ap3
-        embed.addField("Passive 3: " + np3, des);
-      }
-       message.channel.send(embed)
-		}  
-		else {
-			message.channel.send("No Data")
-		}
+		var unit = text.toLowerCase().toTitleCase();
+		unit = nameChange(unit);
+		link = "https://docs.google.com/spreadsheets/u/2/d/e/2PACX-1vQVIjitQOW63fhQOqy31iWCeEWf44BuyVcRIB15YtB1PtoEOoh6rh9Mqw6CresP3ByK7Po6vpCQmqjn/pubhtml#"
+		request(link, function(err, resp, html) {
+			if (!err) {
+				const $ = cheerio.load(html);
+				let siz = $("html body div:nth-child(2) div:nth-child(2) div table tbody").find('tr').length /6;
+				for (var i = 0; i < siz; i++){
+					let name = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 1) + ") td").html()
+					name = te(name)
+					if (name == unit) {
+						let img = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 1) + ") td div img").attr("src")
+						let embed = new Discord.RichEmbed()
+						embed.setThumbnail(img)
+						embed.setTitle(unit)
+						na1 = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 1) + ") td:nth-child(4)").html()
+						sa1 = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 2) + ") td:nth-child(2)").html()
+						sa1 = te(sa1);
+						ra1 = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 2) + ") td:nth-child(3)").html()
+						ra1 = te(ra1);
+						embed.addField(na1, sa1 + "**Range/Cost: **" + ra1);
+						na2 = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 1) + ") td:nth-child(7)").html()
+						sa2 = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 2) + ") td:nth-child(5)").html()
+						sa2 = te(sa2);
+						ra2 = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 2) + ") td:nth-child(6)").html()
+						ra2 = te(ra2);
+						embed.addField(na2, sa2 + "**Range/Cost: **" + ra2);
+						np1 = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 1) + ") td:nth-child(10)").html()
+						if (np1 != "Passive 1: ") {
+							sp1 = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 2) + ") td:nth-child(8)").html()
+							sp1 = te(sp1);
+							embed.addField(np1, sp1);
+						}
+						np2 = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 1) + ") td:nth-child(12)").html()
+						if (np2 != "Passive 2: ") {
+							sp2 = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 2) + ") td:nth-child(10)").html()
+							sp2 = te(sp2);
+							embed.addField(np2, sp2);
+						}
+						np3 = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 1) + ") td:nth-child(14)").html()
+						if (np1 != "Passive 3: ") {
+							sp3 = $("html body div:nth-child(2) div:nth-child(2) div table tbody tr:nth-child(" + (6*i + 2) + ") td:nth-child(12)").html()
+							sp3 = te(sp3);
+							embed.addField(np3, sp3);
+						}
+						message.channel.send(embed)
+						break;
+					}
+					else {
+						message.channel.send("Wrong Name")
+					}
+				}
+			}
+		})
 	}
-}
-function moduleIsAvailable(path) {
-    try {
-        require.resolve(path);
-        return true;
-    } catch (e) {
-        return false;
-    }
 }
 function nameChange(unit) {
   if (name[unit]) unit = name[unit];
 	return unit
+}
+function te(output) {
+	if (output == null) {return null}
+  output = output.replace(/<[^>]*>/g, "\n");
+  output = output.replace(/\n+ /g, "\n");
+  output = he.decode(output);
+  output = output.trim();
+  var arr = output.split('\n');
+  var filtered = arr.filter(function(el) {
+    return el != null && el != '' && el.substring(0,12) != "This ability";
+  });
+  return filtered.join("\n");
 }
 module.exports = Skill;

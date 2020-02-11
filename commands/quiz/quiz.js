6e491bfab831a107d00b8320912d5d14040e9e59
anyabot <52899201+anyabot@users.noreply.github.com>
2020-02-11 23:48:35 +0900

    Update quiz.js

--- commands/quiz/quiz.js
+++ commands/quiz/quiz.js
@@ -61,7 +61,7 @@ function sendembed(units, message, score) {
 				var links = []
 				console.log(link)
 				for (var i = 1; i<10; i++){
-					let img2 = $(".wikia-gallery.wikia-gallery-caption-below.wikia-gallery-position-left.wikia-gallery-spacing-medium.wikia-gallery-border-small.wikia-gallery-captions-left.wikia-gallery-caption-size-medium.inited div:nth-child(" + i +")").html();
+					let img2 = $("#gallery-0 div:nth-child(" + i +") div div a img")..attr("data-src");
             				if (img2) {
 						links.push(img2)
 						console.log(img2)

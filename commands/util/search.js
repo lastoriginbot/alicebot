8d9bfea29aeb2079e64b64b54e595114b54ba52b
anyabot <52899201+anyabot@users.noreply.github.com>
2020-04-18 19:28:49 +0900

    Update search.js

--- commands/util/search.js
+++ commands/util/search.js
@@ -10,7 +10,7 @@ class Search extends commando.Command {
             name: 'search',
             group: 'util2',
             memberName: 'search',
-            description: 'wiki search,
+            description: 'wiki search',
             examples: ['&search alice'],
         });
     }
